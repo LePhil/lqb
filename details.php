a0a60d9f7d87b29a3042582106517e33f962577b
Philipp Christen <philipp.christen@gmail.com>
2016-04-01 23:27:36 +0200

    colors, cleanup, (hidden) legend

--- details.php
+++ details.php
@@ -58,6 +58,13 @@ if ( !isLoggedIn() ) {
 		<h3 class="sectionTitle col_12">Gewichtungen</h3>
 		<div class="col_5">
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<ul id="pieLegend">
+				<li><div class="color"></div><div class="text"></div></li>
+				<li><div class="color"></div><div class="text"></div></li>
+				<li><div class="color"></div><div class="text"></div></li>
+				<li><div class="color"></div><div class="text"></div></li>
+				<li><div class="color"></div><div class="text"></div></li>
+			</ul>
 		</div>
 		<div class="col_7 canvas-holder">
 			<ul class="button-bar monthButtons">
